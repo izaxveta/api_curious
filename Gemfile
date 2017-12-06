aaa6d45707d5d4890983523d0abdff478ca01db0
izaxveta <izavetha@gmail.com>
2017-12-06 16:40:52 -0700

    Added preliminary expectations to feature test

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 gem "figaro"
 gem 'faraday'
 gem 'omniauth-github', '~> 1.1', '>= 1.1.2'
+gem 'pry'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/features/user_can_view_dashboard_spec.rb
+++ spec/features/user_can_view_dashboard_spec.rb
@@ -4,6 +4,41 @@ RSpec.feature 'User can view dashboard' do
   scenario 'as an authorized user' do
     it 'and see user profile' do
 
+
+      repository_count = user.repositories.count
+      stars = user.stars.count
+      followers = user.followers.count
+      following = user.following.count
+
+      within('.nav') do
+        expect(page).to have_link("Overview")
+        expect(page).to have_link("Repositories")
+        expect(page).to have_link("Stars")
+        expect(page).to have_link("Followers")
+        expect(page).to have_link("Following")
+      end
+
+      within('.profile') do
+        expect(page).to have_content("")
+        expect(page).to have_content("")
+      end
+
+      within('.profile-nav') do
+        expect(page).to have_content("")
+        expect(page).to have_content("")
+      end
     end
   end
 end
+
+
+# Authenticate with my github account
+
+# show
+# View basic information about my account (profile pic, number of starred repos, followers, following)
+# View a summary feed of my recent activity (recent commits)
+
+# index
+# View a summary feed of recent activity from users whom I follow
+# View a list of organizations I’m a member of
+# View a list of my repositories
