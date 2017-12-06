5c5697f5a7e9221ce17b509e30d194a2ec6a9087
izaxveta <izavetha@gmail.com>
2017-12-05 20:20:18 -0700

    Refactored and cleaned up SessionsController, pulling set_session into private method, defining session#destroy

--- app/controllers/dashboard_controller.rb
+++
@@ -1,8 +0,0 @@
-class DashboardController < ApplicationController
-  def show
-    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{current_user.token}")
-    auth = JSON.parse(oauth_response.body)
-
-byebug
-  end
-end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,22 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_or_create_by_auth(request.env['omniauth.auth'])
+    set_session
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+
+  def set_session
     if @user
       session[:user_id] = @user.id
       redirect_to dashboard_path
     else
       redirect_to root_path
     end
-  end
-
-  def destroy
-
   end
 end
