23031cc1a3436f45c46adada262c6f288bbb01c8
izaxveta <izavetha@gmail.com>
2017-12-07 21:04:19 -0700

    Eliminated unnecessary Faraday API call

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   def show
-    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{current_user.token}")
-    auth = JSON.parse(oauth_response.body)
-    @user = current_user
+    @github_user = GitHubUser.new(current_user)
   end
 end
