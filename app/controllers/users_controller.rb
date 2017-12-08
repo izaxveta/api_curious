1102a63302604501be9a62a62155ff3fba2ea2f8
izaxveta <izavetha@gmail.com>
2017-12-08 00:41:20 -0700

    Added repository and profile call to the show method of UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @github_user = GitHubUser.new(current_user)
+    @user_profile = @github_user.profile
+    @repos = @github_user.repositories
   end
 end
